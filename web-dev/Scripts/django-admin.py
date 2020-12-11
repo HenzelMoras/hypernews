81d0508b3fee55bdf1d209ff0e8617c1d05963f6
HenzelMoras <henzelmoras@gmail.com>
2020-12-11 20:53:42 +0530

    removed virtual environment

--- web-dev/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zeus\pycharmprojects\hypernews portal\hypernews portal\task\hypernews\web-dev\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
